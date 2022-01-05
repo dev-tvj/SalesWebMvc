226c98985ce40dbb419dcf2f4952aa4207489eaf
Tales <dev.tj@protonmail.com>
2022-01-05 18:44:47 -0400

    Fixed errors and created SeedingService

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -29,15 +29,18 @@ namespace SalesWebMvc
 
             services.AddControllersWithViews();
 
+            services.AddScoped<SeedingService>();
+
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
@@ -46,6 +49,7 @@ namespace SalesWebMvc
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
